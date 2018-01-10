855b08af3b99bf43b86ef45e2f1c76e9def86d6e
“Anthony <anthonyjmcbride@icloud.com>
2018-01-09 18:06:23 -0800

    Detailed styling

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -3,6 +3,6 @@ Spree::Image.class_eval do
     mini: '48x48>', # thumbs under image
     small: '100x100>', # images on category view
     product: '600x600>', # full product image
-    large: '800x800^' # light box image
+    large: '1000x1000' # light box image
   }
 end
